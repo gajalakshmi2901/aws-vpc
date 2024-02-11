7086c90abc424deaf4447ca21ad1de03da534c40
gajalakshmi2901 <115633751+gajalakshmi2901@users.noreply.github.com>
2024-02-11 12:30:51 +0530

    Delete OneDrive - 1CloudHub/Documents directory

--- OneDrive - 1CloudHub/Documents/test-aurora-rds/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = var.bucket
-    key    = var.key
-    region = var.region
-  }
-}
--- OneDrive - 1CloudHub/Documents/test-aurora-rds/backend.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "biw-np-phoenix-s3"
-key    = "rds/terraform.tfstate"
-region= "ap-south-1"
